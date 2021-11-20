c9a4443ebfb95b1d9caf6b92453b65edc65176e5
Dylan Wilbur <dylan.m.w@icloud.com>
2021-11-19 16:16:58 -0800

    more preliminary posts frontend material

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,13 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+// posts options
+export const CREATE_POST = "CREATE_POST";
+export const UPDATE_POST = "UPDATE_POST";
+export const DELETE_POST = "DELETE_POST";
+export const GET_POST = "GET_POST";
+export const TOGGLE_POST_LOADING = "POST_LOADING";
+export const GET_POSTS = "GET_POSTS";
+export const RESET_POST = "RESET_POST";
+export const TOGGLE_POSTS_LOADING = "POSTS_LOADING";
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducers";
 import errorReducer from "./errorReducers";
+import postReducer from "./postReducers";
 export default combineReducers({
   auth: authReducer,
+  post: postReducer,
   errors: errorReducer
+
 });
